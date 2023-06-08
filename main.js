bfefd40b3389d2cff947dc282aa2bf20b970f33b
fozdtx <135956218+fozdtx@users.noreply.github.com>
2023-06-08 13:38:15 +0530

    Uploaded files
    at 13:38pm

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log('Hello World!');
+
+
+function download_01(){
+  window.open("https://drive.google.com/file/d/1nH-1JokBVUFJZQa2Z2GvLxNz0qxLC_QD/view?usp=drivesdk");
+}
+
+function download_02(){
+  window.open("https://drive.google.com/file/d/1niUfUp2S2dSSuD3pCG-0n8OaNLmw0wMp/view?usp=drivesdk");
+}
+
+function download_03(){
+  window.open("https://drive.google.com/file/d/1qdPK2rviIirp3LknuN_HMX2qtBQPPHUn/view?usp=drivesdk");
+
+
+function donation(){
+  window.open("donate.html");
+}
+
+function learn(){
+  window.open("Learn.html")
+}
